a52813125a9cbc29756e9d14b037324d044bafa0
ntndlcrt <adelcourte.pro@gmail.com>
2024-10-21 14:16:24 +0200

    Remove tailwind

--- bud.config.js
+++ bud.config.js
@@ -74,7 +74,5 @@ export default async (app) => {
         customFontSize: false,
       },
     })
-    .useTailwindColors()
-    .useTailwindFontFamily()
-    .useTailwindFontSize()
+    .enable()
 }
