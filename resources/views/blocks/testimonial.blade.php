2d94f0c80d7475dc033a41bc541ed2ee0df9b433
ntndlcrt <adelcourte.pro@gmail.com>
2024-10-21 14:35:45 +0200

    Add first block

---
+++ resources/views/blocks/testimonial.blade.php
@@ -0,0 +1,14 @@
+{{--
+  Title: Testimonial
+  Description: Customer testimonial
+  Category: formatting
+  Icon: admin-comments
+  Keywords: testimonial quote
+  PostTypes: page post
+  SupportsMode: false
+  SupportsMultiple: true
+--}}
+
+<section class="b-testimonial">
+  <h1>@field('title')</h1>
+</section>
